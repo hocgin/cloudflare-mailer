69ce7b63e047fb948b8d7b6428154551d7712c0a
hocgin <hocgin@gmail.com>
2023-09-03 20:12:53 +0800

    dev

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,25 @@
-export default {
-	async fetch(request: any) {
-		console.log('请求');
-		let sendReq = new Request('https://api.mailchannels.net/tx/v1/send', {
-			method: 'POST',
-			headers: {
-				'content-type': 'application/json'
-			},
-			body: JSON.stringify({
-				personalizations: [{
-					to: [{ email: 'hocgin@gmail.com', name: 'Test Recipient' }]
-				}],
-				from: {
-					email: 'sender@hocgin.com',
-					name: 'Workers - MailChannels integration'
-				},
-				subject: 'Look! No servers',
-				content: [
-					{
-						type: 'text/plain',
-						value: 'And no email service accounts and all for free too!'
-					}
-				]
-			})
-		});
+import Email from 'vercel-email';
 
-		let res = await fetch(sendReq);
-		return res;
+export default {
+	async fetch(req: any) {
+		let pathname = new URL(req.url).pathname;
+		console.log('url', pathname);
+		if (pathname !== `/api`) {
+			return new Response(JSON.stringify({
+				success: false,
+				message: '404'
+			}));
+		}
+		try {
+			await Email.send((await req.json()));
+			return new Response(JSON.stringify({
+				success: true
+			}));
+		} catch (e: any) {
+			return new Response(JSON.stringify({
+				success: false,
+				message: e?.message
+			}));
+		}
 	}
 };
